d3a37354fcbfbb00765675de9584c2f82b7cb176
천나영 <102145415+cheonnayeong@users.noreply.github.com>
2023-05-20 21:56:34 +0900

    control_input_process.php

---
+++ 0516/control_input_process.php
@@ -0,0 +1,21 @@
+<?php
+	date_defalut_timezone_set('Asia/Seoul');
+	$did = $_POST['did'];
+	$pin = $_POST['pin'];
+      $cmd = $_POST['cmd'];
+	$date = $_POST("Y-m-d H:i:s",time());
+
+   //MYSQL연결한다
+     $conn = mysqli_connect('localhost', 'root', '','bssm2_3');
+    //데이터를 insert하는 SQL쿼리를 작성
+    $query = "insert into control(did,pin,cmd,checked,date) values('".$did."',".$pin.", ".$cmd.", 0,'".$date."');";
+    //SQL쿼리를 실행
+    $result = mysqli_query($conn, $query);
+    //실행결과 확인
+    if($result){
+     echo "성공";
+    }else{
+     echo "실패";
+    }
+?>
+<meta http-equiv="refresh" content="0; url=input.php">
